99b62452ee8ede3f8163c2d74a824475b950cf31
Forthtilliath <forth@live.fr>
2024-07-22 08:08:08 +0200

    add layout parts components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: process.env.CI
+    ? "https://cda-showcase.vercel.app"
+    : "http://localhost:4321",
+  integrations: [react(), tailwind()],
 });
